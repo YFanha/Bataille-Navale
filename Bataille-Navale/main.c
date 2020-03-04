78d64bac3b3435f063605cbf5b7dadbcdbeb5874
FANHA-DIAS Yann <Yann.FANHA-DIAS@cpnv.ch>
2020-03-04 15:01:46 +0100

    Création du projet sur Clion

---
+++ Bataille-Navale/main.c
@@ -0,0 +1,15 @@
+/**
+ * \project Bataille Navale
+ * \author Yann Fanha
+ * \date 04.03.2020
+ * \version 0.1
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <Windows.h>
+
+int main() {
+    printf("Hello, World!\n");
+    return 0;
+}
