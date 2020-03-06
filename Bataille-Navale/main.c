b5bc653c87da914c54e27334c9264808fbe8e803
FANHA-DIAS Yann <Yann.FANHA-DIAS@cpnv.ch>
2020-03-06 15:07:04 +0100

    ajout de la fonction menu + quitter + ajouts de commentaire

--- Bataille-Navale/main.c
+++ Bataille-Navale/main.c
@@ -8,8 +8,112 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <Windows.h>
+#include <string.h>
+#include <time.h>
+#pragma execution_character_set("utf-8") //Accents
+
+#define ROW 10
+#define column 10
+
+/**
+ * \author Yann Fanha
+ * \date 06.03.2020
+ * @return 0
+ */
+int quitter(){
+    return 0;
+}
+
+/**
+ * \author Yann Fanha
+ * \date 06.03.2020
+ * \description Afficher le titre
+ */
+void afficherTitre(){
+    printf("/---------------Bataille Navale---------------/\n\n");
+}
+
+/**
+ * \author Yann Fanha
+ * \date 06.03.2020
+ * \description fonction qui permet de jouer
+ */
+void jouer(){
+}
+
+/**
+ * \author Yann Fanha
+ * \date 06.03.2020
+ * \description Affiche l'aide du jeu
+ */
+void aide(){
+    FILE*fichierAide = NULL;
+    int aide;
+
+    fichierAide = fopen("DataBase\\documentation.txt", "r");
+
+    if(fichierAide != NULL){
+        afficherTitre();
+        do{
+            aide = fgetc(fichierAide);
+
+            printf("%c", aide);
+        }while(aide != EOF);
+
+        fclose(fichierAide);
+
+    } else {
+        printf("L'aide n'est actuellement pas disponible. Envoyez un mail à l'adresse Yann.FANHA-DIAS@cpnv.ch pour obtenir de l'aide.");
+    }
+}
+
+/**
+ * \author Yann Fanha
+ * \date 06.03.2020
+ * \description Affiche le menu et récupere le choix de l'user
+ * @param choix
+ */
+void menu(int choix){
+    afficherTitre();
+    printf("1 - Jouer");
+    printf("\n2 - S'enregistrer");
+    printf("\n3 - Scores");
+    printf("\n4 - Aide");
+    printf("\n5 - Quitter\n->");
+    scanf("%d", &choix);
+
+    //appeller la bonne fonction
+    switch(choix){
+        case 1:jouer();
+            break;
+
+        case 2:
+            break;
+
+        case 3:
+            break;
+
+        case 4: aide();
+            break;
+
+        case 5: quitter();
+            break;
+
+        default: system("cls");
+                menu(choix);
+                break;
+    }
+}
+
 
 int main() {
-    printf("Hello, World!\n");
-    return 0;
+    int choix = 0;
+    SetConsoleOutputCP(65001);//Accents
+
+    //Appeller la fonction du menu
+    menu(choix);
+
+
+    printf("\n\n");
+
 }
