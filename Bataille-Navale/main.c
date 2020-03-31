72693e60ff6f1ce813ae13c8607b51abaf89b595
YFanha <61124069+YFanha@users.noreply.github.com>
2020-03-31 22:40:53 +0200

    2 Nouvelles fonctions, nouveaux fichier, correction de problèmes
    Ajout de la fonction d'inscription des scores + correction d'un problème qui faisait buguer la fin du jeu si on tirait deux fois sur la mêmes case.
    
    Création des fichiers (.txt)pour stocker les stockers les coordonnées (une grille par fichier).
    
    Ajout de la fonction "grilleAleatoire" qui servira de fonction qui va lire un fichier et placer les bateux dans la grilleAttaque. Mais beaucoup de problème restant: - ne trouvais pas les bon caractère, probleme dans la gestion des données recue., Faisait les choses a double..
    
    ajout de la stratégie du projet (stratégie.docx)
    
    Correction du problème des caractères dans la fonction "grilleAleatoire".
    
    Correction du problème de la gestion de données reçu et le problème des doublons. Il reste un ptoblème; PARFOIS, le bateaux deux possède trois cases (devrait en possèder deux)

--- Bataille-Navale/main.c
+++ Bataille-Navale/main.c
@@ -11,6 +11,7 @@
 #include <string.h>
 #include <time.h>
 #include <ctype.h>
+#include <stdbool.h>
 #pragma execution_character_set("utf-8") //Accents
 
 #define SIZE_MAX_PSEUDO 30
@@ -18,17 +19,23 @@
 #define SIZE_COLUMN 10 //Taille MAX des colunnes de la grille
 #define TETE_GRILLE  {'A','B','C','D','E','F','G','H','I','J'}
 #define GRILLE_SHOOT {{'-','-','-','-','-','-','-','-','-','-'},{'-','-','-','-','-','-','-','-','-','-'},{'-','-','-','-','-','-','-','-','-','-'},{'-','-','-','-','-','-','-','-','-','-'},{'-','-','-','-','-','-','-','-','-','-'},{'-','-','-','-','-','-','-','-','-','-'},{'-','-','-','-','-','-','-','-','-','-'},{'-','-','-','-','-','-','-','-','-','-'},{'-','-','-','-','-','-','-','-','-','-'},{'-','-','-','-','-','-','-','-','-','-'}}
-#define GRILLE_DE_JEU {{'-','-','2','2','-','-','-','5','-','-'},{'-','-','-','-','-','-','-','5','-','-'},{'-','-','-','-','-','-','-','5','-','-'},{'-','-','-','-','-','-','-','5','-','-'},{'3','3','3','-','-','-','-','5','-','-'},{'-','-','-','-','-','-','-','-','-','-'},{'-','4','-','-','-','-','-','-','-','-'},{'-','4','-','-','-','-','-','-','-','-'},{'-','4','-','-','-','1','1','1','-','-'},{'-','4','-','-','-','-','-','-','-','-'}}
+#define TAILLE_MAX_COORDONNEE 3
+#define NOMBRE_GRILLE_MAX 5
+#define PREMIERE_CORDONNEE_GRILLE 2
+#define LONGEUR_MAX_LIGNE 50
+#define LONG_MAX_NOM_FICHIER 100
+#define NOM_FICHIER_GRILLE {{"DataBase\\Grille\\grille1.txt"},{"DataBase\\Grille\\grille2.txt"},{"DataBase\\Grille\\grille3.txt"},{"DataBase\\Grille\\grille4.txt"},{"DataBase\\Grille\\grille5.txt"}}
+#define MAX 15
 
 typedef struct {;
     char x;
     int y;
 }coordonnee;
 
-
-char pseudo[SIZE_MAX_PSEUDO]; //(Car je ne trouvais pas comment return une chaîne de caractère)
-int quit = 0; //Variable pour dire si oui ou non on sort du programme (1=OUI, 0=NON)
-int dejaEnregistrer = 0; //Pseudo si on est déjà enregistrer ou non
+char grilleAttaque[SIZE_COLUMN][SIZE_ROW];
+char pseudo[SIZE_MAX_PSEUDO] = "0"; //(Car je ne trouvais pas comment return une chaîne de caractère)
+bool quit = false; //Variable pour dire si oui ou non on sort du programme (1=OUI, 0=NON)
+bool dejaEnregistrer = false; //Dire si oui ou non on est déjà enregistrer
 
 
 /**
@@ -41,6 +48,89 @@ void afficherTitre(){
     printf("/---------------Bataille Navale---------------/\n\n");
 }
 
+void remettreGrilleZero(){
+    for (int j = 0; j < SIZE_COLUMN; j++){
+        for (int  t = 0;  t < SIZE_ROW; t++) {
+            grilleAttaque[j][t] = ' ';
+        }
+    }
+}
+
+void grilleAleatoire(){
+    int numeroDeGrille;
+    int character = -1;
+    char ligne[LONGEUR_MAX_LIGNE];
+    char numeroBateau =0;
+    char grille[NOMBRE_GRILLE_MAX][LONG_MAX_NOM_FICHIER] = NOM_FICHIER_GRILLE;
+    char grilleVide[SIZE_COLUMN][SIZE_ROW] = GRILLE_SHOOT;
+    coordonnee coordonneeXY = {0,0};
+
+
+    //remet la grille a zero
+    remettreGrilleZero();
+
+    FILE*fichierGrille = NULL;
+    //numeroDeGrille = rand() % NOMBRE_GRILLE_MAX + 1;
+    numeroDeGrille = 2;
+    fichierGrille = fopen(grille[numeroDeGrille], "r");
+
+
+
+    if(fichierGrille != NULL){
+        printf("%s\n", grille[numeroDeGrille]);
+       do{
+            character = fgetc(fichierGrille);
+            fgets(ligne, sizeof(ligne), fichierGrille);
+
+            numeroBateau = ligne[0];
+
+
+            printf("Bateau no %c\n", numeroBateau);
+
+            for (int i = PREMIERE_CORDONNEE_GRILLE; i <= MAX; i++) {
+                if (isdigit(ligne[i])) {
+                    coordonneeXY.y = ligne[i] - '0';
+                    printf("%d-%d\n", coordonneeXY.y, coordonneeXY.x);
+                    grilleAttaque[coordonneeXY.y][coordonneeXY.x] = numeroBateau;
+                } else if (isalpha(ligne[i])) {
+                    coordonneeXY.x = ligne[i];
+                    coordonneeXY.x = toupper(coordonneeXY.x);
+                    coordonneeXY.x -= 'A';
+                }
+            }
+
+        }while(character != EOF);
+
+    } else {
+        grilleAleatoire();
+    }
+
+
+    fclose(fichierGrille);
+    system("pause");
+}
+
+/**
+ * \author Yann Fanha
+ * \date 25.03.2020
+ * \description Inscrire le pseudo ainsi que le nombre de louper dans le fichier qui sert de base de données
+ */
+void inscrireLeScore(int louper){
+    FILE*fichierScore = NULL;
+    char characater;
+
+    fichierScore = fopen("DataBase\\score.txt", "a");
+
+    fprintf(fichierScore,"%s %20d\n", pseudo, louper);
+
+    fclose(fichierScore);
+}
+
+/**
+ * \author Yann Fanha
+ * \date 23.03.2020
+ * \description Afficher les scores qui sont enregistrer dans le fichier qui sert de base de données
+ */
 void afficherLesScores(){
     FILE*fichierScore = NULL;
     int character;
@@ -72,11 +162,12 @@ void afficherLesScores(){
 void authentification(){
     int changePseudo;
 
-    if(dejaEnregistrer == 0){
+    if((void *) dejaEnregistrer == NULL){
         printf("Entrez votre nom : ");
         scanf("%s", pseudo);
+        dejaEnregistrer = true;
     } else {
-        printf("Vous êtes déjà enregistrer sous le pseudo de %s. Voulez-vous changer de pseudo ? (o/n)", pseudo);
+        printf("Vous êtes déjà enregistrer sous le pseudo de %s. Voulez-vous changer de pseudo ? (oui=1 / non=0)", pseudo);
         scanf("%d", &changePseudo);
 
         if(changePseudo == 1) {
@@ -118,34 +209,75 @@ void afficherGrille(char teteDeGrille[SIZE_ROW], char grilleDeShoot[SIZE_COLUMN]
 
 /**
  * \author Yann Fanha
+ * \date 27.03.2020
+ * \description Fonctions qui concatène les deux entiers pour la coordonnée vertical
+ * @param nombre1
+ * @param nombre2
+ * @return coordonneeY
+ */
+int concat(int nombre1, int nombre2){
+
+    char s1[20];
+    char s2[20];
+
+    // convertir les deux entier en chaine
+    sprintf(s1, "%d", nombre1);
+    sprintf(s2, "%d", nombre2);
+
+    //assembler les deux caratère
+    strcat(s1, s2);
+
+    //Transformer les deux caractère en entier
+    int coordonneeY = atoi(s1);
+
+    return coordonneeY;
+}
+
+/**
+ * \author Yann Fanha
  * \16.03.2020
- * \description Fonction qui permet de demander la valeur de la coordonnée horizontal
+ * \dernière modification le 27.03.2020
+ * \description Fonction qui permet de demander les coordonnées du shoot
  * @return coordonneeXY
  */
 coordonnee demandeCoordonnee(){
-    coordonnee coordonneeXY;
-    do {
-        printf("\nHorizontal (en lettre): ");
-        scanf("%c", &coordonneeXY.x);
+    coordonnee coordonneeXY = {0 , 0};
+    char coordonnee[TAILLE_MAX_COORDONNEE] = "\0";
 
-        //si la valeur est un nombre
-        if ( isdigit(coordonneeXY.x )){
-            printf("Rentrez la coordonnée en lettre");
+
+    do {
+        int n1 = -20;
+        int n2 = -20;
+        printf("\nCoordonnées :");
+        scanf("%s", coordonnee);
+
+        for (int i = 0; i < TAILLE_MAX_COORDONNEE; ++i) {
+            if( isdigit(coordonnee[i]) ) {
+                if (n1 == -20) {
+                    n1 = coordonnee[i] - '0';
+                } else {
+                    n2 = coordonnee[i] - '0';
+                }
+
+            } else if ( isalpha(coordonnee[i]) ){
+                coordonneeXY.x = coordonnee[i];
+            }
         }
 
-    }while ( !isalpha(coordonneeXY.x) || (coordonneeXY.x > 'j' && coordonneeXY.x <= 'z') || (coordonneeXY.x > 'J' && coordonneeXY.x <= 'Z'));
+        if(n2 == -20) {
+            coordonneeXY.y = n1 -1;
+        } else {
+            coordonneeXY.y = concat(n1, n2) - 1;
+        }
 
-    coordonneeXY.x = toupper(coordonneeXY.x);
-    coordonneeXY.x -= 'A';
+        //convertir la lettre rentrée en majuscule
+        coordonneeXY.x = toupper(coordonneeXY.x);
 
 
-    do{
-        printf("\nVertical  : ");
-        fflush(stdin);
-        scanf("%d", &coordonneeXY.y);
-    }while(coordonneeXY.y < 1 || coordonneeXY.y > 10);
+    }while( coordonneeXY.y > 9 || coordonneeXY.y < 0 || coordonneeXY.x > 'J');
 
-    coordonneeXY.y -= 1;
+    coordonneeXY.x -= 'A'; //Prendre la bonne valeur décimal
+
     return coordonneeXY;
 }
 
@@ -157,8 +289,9 @@ coordonnee demandeCoordonnee(){
 void jouer(){
     char teteDeGrille[SIZE_ROW]  = TETE_GRILLE; //POUR L'AFFICHE DE LA TETE DE TITRE
     char grilleDeShoot[SIZE_COLUMN][SIZE_ROW] = GRILLE_SHOOT; //Grille que l'utilisateur pourra voir pour savoir ou tirer
-    char grilleAttaque[SIZE_COLUMN][SIZE_ROW] = GRILLE_DE_JEU; //Grille qui contiendra les coordonnées des bateaux
 
+    //Prendre un grille aleatoire
+    grilleAleatoire();
 
     //Déclaration des variable pour savoir si un bateau a été coulé
     int bateau5 = 5, //Bateau qui occupe 5 place
@@ -169,68 +302,72 @@ void jouer(){
 
         int nbrtirs = 0;
         int louper = 0;
+        int nbrBateauCouler = 0;
         coordonnee coordonneeXY={0,0};
 
 
-    do {
+    /*do {
 
         system("cls");
 
+        if(grilleDeShoot[coordonneeXY.y][coordonneeXY.x] == 'T' || grilleDeShoot[coordonneeXY.y][coordonneeXY.x] == 'X'){
+                printf("Vous avez déjà tiré sur cette case\n");
+            } else {
+                switch (grilleAttaque[coordonneeXY.y][coordonneeXY.x]){
+                    case '1':
+                        printf("\nUn bateau a été touché.\n");
+                        grilleDeShoot[coordonneeXY.y][coordonneeXY.x] = 'T';
+                        bateau1 -= 1;
+                        if(bateau1 == 0){
+                            printf("Un bateau a été coulé\n");
+                        }
+                        break;
 
-        switch (grilleAttaque[coordonneeXY.y][coordonneeXY.x]){
-            case '1':
-                printf("\nUn bateau a été touché.\n");
-                grilleDeShoot[coordonneeXY.y][coordonneeXY.x] = 'T';
-                bateau1 -= 1;
-                if(bateau1 == 0){
-                    printf("Un bateau a été coulé\n");
-                }
-                break;
+                    case '2':
+                        printf("\nUn bateau a été touché.\n");
+                        grilleDeShoot[coordonneeXY.y][coordonneeXY.x] = 'T';
+                        bateau2 -= 1;
+                        if(bateau2 == 0){
+                            printf("Un bateau a été coulé\n");
+                        }
+                        break;
 
-            case '2':
-                printf("\nUn bateau a été touché.\n");
-                grilleDeShoot[coordonneeXY.y][coordonneeXY.x] = 'T';
-                bateau2 -= 1;
-                if(bateau2 == 0){
-                    printf("Un bateau a été coulé\n");
-                }
-                break;
+                    case '3':
+                        printf("\nUn bateau a été touché.\n");
+                        grilleDeShoot[coordonneeXY.y][coordonneeXY.x] = 'T';
+                        bateau3 -= 1;
+                        if(bateau3 == 0){
+                            printf("Un bateau a été coulé\n");
+                        }
+                        break;
 
-            case '3':
-                printf("\nUn bateau a été touché.\n");
-                grilleDeShoot[coordonneeXY.y][coordonneeXY.x] = 'T';
-                bateau3 -= 1;
-                if(bateau3 == 0){
-                    printf("Un bateau a été coulé\n");
-                }
-                break;
+                    case '4':
+                        printf("\nUn bateau a été touché.\n");
+                        grilleDeShoot[coordonneeXY.y][coordonneeXY.x] = 'T';
+                        bateau4 -= 1;
+                        if(bateau4 == 0){
+                            printf("Un bateau a été coulé\n");
+                        }
+                        break;
 
-            case '4':
-                printf("\nUn bateau a été touché.\n");
-                grilleDeShoot[coordonneeXY.y][coordonneeXY.x] = 'T';
-                bateau4 -= 1;
-                if(bateau4 == 0){
-                    printf("Un bateau a été coulé\n");
-                }
-                break;
+                    case '5':
+                        printf("\nUn bateau a été touché.\n");
+                        grilleDeShoot[coordonneeXY.y][coordonneeXY.x] = 'T';
+                        bateau5 -= 1;
+                        if(bateau5 == 0){
+                            printf("Un bateau a été coulé\n");
+                        }
+                        break;
 
-            case '5':
-                printf("\nUn bateau a été touché.\n");
-                grilleDeShoot[coordonneeXY.y][coordonneeXY.x] = 'T';
-                bateau5 -= 1;
-                if(bateau5 == 0){
-                    printf("Un bateau a été coulé\n");
-                }
-                break;
-
-            default:
-                if(nbrtirs > 0) {
-                    printf("\nAucun bateau n'a été touché.\n");
-                    grilleDeShoot[coordonneeXY.y][coordonneeXY.x] = 'X';
-                    louper += 1;
-                }
-                break;
-        }
+                    default:
+                        if(nbrtirs > 0) {
+                            printf("\nAucun bateau n'a été touché.\n");
+                            grilleDeShoot[coordonneeXY.y][coordonneeXY.x] = 'X';
+                            louper += 1;
+                        }
+                        break;
+                    }
+            }
 
         //afficher la grille
         afficherGrille(teteDeGrille, grilleDeShoot);
@@ -249,7 +386,31 @@ void jouer(){
         printf("\n");
         printf("\n");
 
-    }while (bateau2 != 0 || bateau3 != 0 || bateau4 != 0 || bateau5 != 0 || bateau1 != 0);
+    }while (bateau2 != 0 || bateau3 != 0 || bateau4 != 0 || bateau5 != 0 || bateau1 != 0);*/
+
+    //inscrireLeScore(louper);
+
+    for (int lettre = 0; lettre < SIZE_ROW; lettre++) { //Afficher l'en-tete des colonnes
+        if (lettre == 0) {
+            printf("%8c", teteDeGrille[lettre]);
+        } else {
+            printf("%7c", teteDeGrille[lettre]);
+        }
+    }
+    printf("\n");
+    for (int colonne = 0; colonne < SIZE_COLUMN; colonne++) {
+        printf("%d", colonne + 1);
+        for (int ligne = 0; ligne < SIZE_ROW; ligne++) {
+            if (colonne == 9 && ligne == 0) { //Bien aligner les tirets quand le nombre tout a gauche == 10
+                printf("%6c", grilleAttaque[colonne][ligne]);
+            } else {
+                printf("%7c", grilleAttaque[colonne][ligne]);
+            }
+        }
+        printf("\n");
+    }
+
+    system("pause");
 }
 
 /**
@@ -300,11 +461,15 @@ void menu(int choix){
 
     system("cls");
 
-    char nom[25] = "Yann";
-
     //appeller la bonne fonction
     switch(choix){
-        case 1:jouer();
+        case 1:
+            if((void *) dejaEnregistrer == NULL){
+                authentification();
+                jouer();
+            } else{
+                jouer();
+            }
             break;
 
         case 2: authentification();
@@ -316,7 +481,10 @@ void menu(int choix){
         case 4: aide();
             break;
 
-        case 5: quit = 1; //Pour définir que l'on veut bien quitter le programme
+        case 5: quit = TRUE; //Pour définir que l'on veut bien quitter le programme
+            break;
+
+        case 6: grilleAleatoire();
             break;
 
         default: system("cls");
@@ -337,8 +505,7 @@ int main() {
 
     printf("\n\n");
 
-    return 0;
-    if(quit == 0){
+    if((void *) quit == false){
         return main();
     } else {
         return 0;
